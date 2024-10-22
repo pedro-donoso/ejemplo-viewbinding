11ee5f0ba82e3b8daeaddf0ee99559357df86f4c
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-10-21 22:42:04 -0300

    creo metodo viewbinding

--- app/src/main/java/com/example/ejemplo_viewbinding/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo_viewbinding/MainActivity.kt
@@ -2,46 +2,23 @@ package com.example.ejemplo_viewbinding
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ejemplo_viewbinding.ui.theme.EjemploviewbindingTheme
+import com.example.ejemplo_viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            EjemploviewbindingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn.setOnClickListener {
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EjemploviewbindingTheme {
-        Greeting("Android")
-    }
-}
+
